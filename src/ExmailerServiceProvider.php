a0290384c8217e3a051d1317a9311ec35eb95b81
5ar7or1u5 <58537166+5ar7or1u5@users.noreply.github.com>
2020-06-02 19:01:09 -0400

    Update ExmailerServiceProvider.php

--- src/ExmailerServiceProvider.php
+++ src/ExmailerServiceProvider.php
@@ -1,1 +1,14 @@
+<?php
 
+namespace  5ar7or1u5/Exmailer; 
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class ExmailerServiceProvider extends ServiceProvider
+{
+    protected $listen = [
+        'Illuminate\Mail\Events\MessageSending' => [
+            __DIR__.'\Exmailer',
+        ],
+    ];
+}
