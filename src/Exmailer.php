2667dd79cf8c38584202e6262d0a214b9624981f
5ar7or1u5 <58537166+5ar7or1u5@users.noreply.github.com>
2020-06-02 19:06:56 -0400

    Update Exmailer.php

--- src/Exmailer.php
+++ src/Exmailer.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace  5ar7or1u5/Exmailer; 
+
+use App\User;
+use Illuminate\Mail\Events\MessageSending;
+use Illuminate\Support\Facades\App;
+
+class Exmailer
+{
+    public function handle( MessageSending $event )
+    {
+        if(App::environment() !== 'production'){
+            return false;
+        }
+    }
+}
